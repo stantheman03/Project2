2d482b1bbef0a0ab93bd66c3426e04f3bf5f56ee
Stanley Philip <Stanley Philip>
2018-11-14 18:24:01 -0500

    first

--- Unsolved/models/example.js
+++ Unsolved/models/example.js
@@ -1,7 +1,32 @@
-module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
-  });
-  return Example;
-};
+module.exports = function(sequelize,Datatypes){
+  var Coffee = sequelize.define('Coffee',{
+      name:{
+          type: Datatypes.STRING,
+          allowNull:false,
+          validate:{
+              isAlpha:true
+          }
+      },
+      CoffeeType:{
+          type:Datatypes.STRING,
+          allowNull:false
+      },
+      roast:{
+          type:Datatypes.STRING,
+          allowNull:false
+      },
+      createdAt:{
+          type:Datatype.INTERGER,
+          allowNull:false
+      },
+      updatedAt:{
+          type:Datatype.INTERGER,
+          allowNull:false
+      }
+  })
+  Coffee.associate = function(models){
+
+  }
+
+  return Coffee;
+}
--- Unsolved/routes/apiRoutes.js
+++ Unsolved/routes/apiRoutes.js
@@ -1,24 +1,52 @@
-var db = require("../models");
+var db = require('../models');
 
-module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
+module.exports = function(app){
+    app.get('/api/coffee/',function(req,res){
+        db.Coffee.findAll({}).then(function(dbCoffee){
+           res.render('index',{Coffee:dbCoffee})
+        })
+    })
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
+app.get('api/coffee/:id',function(req,res){
+    db.Coffee.findOne({
+        where:{
+            id:req.params.id
+        }
+    }).then(function(dbCoffee){
+        res.render('index',{Coffee:dbCoffee})
+    })
+});
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
-};
+app.post('/api/coffee',function(req,res){
+   db.Coffee.create({
+       name:req.body.name,
+       CoffeeType:req.body.CoffeeType,
+       roast:req.body.roast
+   }).then(function(dbCoffee){
+       res.redirect('/api/coffee')
+   })
+})
+
+app.put('/api/coffee',function(req,res){
+    db.Coffee.update(req.body,
+        {
+            where:{
+                id:req.body.id
+            }
+        }).then(function(dbCoffee){
+            res.redirect('/api/coffee')
+        })
+})
+
+app.delete('/api/coffee/:id',function(req,res){
+    db.Coffee.destroy({
+        where:{
+            id:req.params.id
+        }
+    }).then(function(dbCoffee){
+        res.redirect('/api/coffee') 
+    })
+})
+
+
+}
